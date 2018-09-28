c509438e4e6c0578b1ae38ffe567dc59277bc938
Ismael <ismael98r@hotmail.com>
2018-09-28 13:33:58 +0200

    V1.2

--- app/src/main/java/es/iessaladillo/ismaelraqi/irp_pr02_exchange/MainActivity.java
+++ app/src/main/java/es/iessaladillo/ismaelraqi/irp_pr02_exchange/MainActivity.java
@@ -1,9 +1,11 @@
 package es.iessaladillo.ismaelraqi.irp_pr02_exchange;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
